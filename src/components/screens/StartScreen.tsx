3bc2bdb0c0f8624a7eab65a3e1c59e6b92b3ec09
iRod <irod@clawdbot.local>
2026-02-03 18:35:19 +0000

    feat: Add RJ logo to landing page

--- src/components/screens/StartScreen.tsx
+++ src/components/screens/StartScreen.tsx
@@ -59,7 +59,7 @@ export function StartScreen({ onStart, isExiting }: StartScreenProps) {
           }`}
         >
           <Image
-            src="/images/mascots/rj-logo.webp"
+            src="/images/rj-logo.png"
             alt="RJ Logo"
             width={200}
             height={200}
